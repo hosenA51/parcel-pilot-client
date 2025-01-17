f6cd75f37b66de6a5ea825fb2f47f8bfdb594dea
hosenA51 <hosenahmed2552@gmail.com>
2025-01-17 09:37:00 +0600

    added Navbar section

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,95 +1,114 @@
 import Container from '../Container'
-import { AiOutlineMenu } from 'react-icons/ai'
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 // import useAuth from '../../../hooks/useAuth'
 // import avatarImg from '../../../assets/images/placeholder.jpg'
 import logo from '../../../assets/logo1.png'
+import { Button } from '@/components/ui/button'
+import { MdNotificationAdd } from "react-icons/md";
+import './Navbar.css'
 const Navbar = () => {
-//   const { user, logOut } = useAuth()
-  const [isOpen, setIsOpen] = useState(false)
+    //   const { user, logOut } = useAuth()
+    const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <div className='fixed w-full bg-white z-10 shadow-sm'>
-      <div className='py-4 border-b-[1px]'>
-        <Container>
-          <div className='flex flex-row  items-center justify-between gap-3 md:gap-0'>
-            {/* Logo */}
-            <Link to='/'>
-              <img src={logo} alt='logo' width='100' height='100' />
-            </Link>
-            {/* Dropdown Menu */}
-            <div className='relative'>
-              <div className='flex flex-row items-center gap-3'>
-                {/* Dropdown btn */}
-                <div
-                  onClick={() => setIsOpen(!isOpen)}
-                  className='p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'
-                >
-                  <AiOutlineMenu />
-                  <div className='hidden md:block'>
-                    {/* Avatar */}
-                    {/* <img
-                      className='rounded-full'
-                      referrerPolicy='no-referrer'
-                      src={user && user.photoURL ? user.photoURL : avatarImg}
-                      alt='profile'
-                      height='30'
-                      width='30'
-                    /> */}
-                  </div>
-                </div>
-              </div>
-              {isOpen && (
-                <div className='absolute rounded-xl shadow-md w-[40vw] md:w-[10vw] bg-white overflow-hidden right-0 top-12 text-sm'>
-                  <div className='flex flex-col cursor-pointer'>
-                    <Link
-                      to='/'
-                      className='block md:hidden px-4 py-3 hover:bg-neutral-100 transition font-semibold'
-                    >
-                      Home
-                    </Link>
-
-                    {user ? (
-                      <>
-                        <Link
-                          to='/dashboard'
-                          className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
-                        >
-                          Dashboard
+    return (
+        <div className='fixed w-full bg-white z-10 shadow-sm'>
+            <div className='py-4 border-b-[1px]'>
+                <Container>
+                    <div className='flex flex-row  items-center justify-between gap-3 md:gap-0'>
+                        {/* Logo */}
+                        <Link to='/'>
+                            <div className='flex gap-2 items-center'>
+                                <img src={logo} alt='logo' width='40' height='40' />
+                                <h2 className='text-3xl font-bold text-[#ca6602]'>ParcelPilot</h2>
+                            </div>
                         </Link>
-                        <div
-                          onClick={logOut}
-                          className='px-4 py-3 hover:bg-neutral-100 transition font-semibold cursor-pointer'
-                        >
-                          Logout
+                        <div className='flex items-center gap-5 justify-end'>
+                            <NavLink
+                                to="/"
+                                className={({ isActive }) =>
+                                    isActive
+                                        ? "text-lg font-semibold underline text-[#ca6602]"
+                                        : "text-lg font-semibold"
+                                }
+                            >
+                                Home
+                            </NavLink>
+                            <p className='text-2xl text-[#ca6602] font-semibold cursor-pointer' ><MdNotificationAdd /></p>
+                            {/* Dropdown Menu */}
+                            {isOpen ?
+                                <div className='relative'>
+                                    <div className='flex flex-row items-center gap-3'>
+                                        {/* Dropdown btn */}
+                                        <div
+                                            onClick={() => setIsOpen(!isOpen)}
+                                            className='p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'
+                                        >
+                                            <div className='hidden md:block'>
+                                                {/* Avatar */}
+                                                <img
+                                                    className='rounded-full'
+                                                    referrerPolicy='no-referrer'
+                                                    src={user && user.photoURL}
+                                                    alt='profile'
+                                                    height='30'
+                                                    width='30'
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    {isOpen && (
+                                        <div className='absolute rounded-xl shadow-md w-[40vw] md:w-[10vw] bg-white overflow-hidden right-0 top-12 text-sm'>
+                                            <div className='flex flex-col cursor-pointer'>
+                                                <Link
+                                                    to='/'
+                                                    className='block md:hidden px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                                                >
+                                                    Home
+                                                </Link>
+
+                                                {user ? (
+                                                    <>
+                                                        <Link
+                                                            to='/dashboard'
+                                                            className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                                                        >
+                                                            Dashboard
+                                                        </Link>
+                                                        <div
+                                                            onClick={logOut}
+                                                            className='px-4 py-3 hover:bg-neutral-100 transition font-semibold cursor-pointer'
+                                                        >
+                                                            Logout
+                                                        </div>
+                                                    </>
+                                                ) : (
+                                                    <>
+
+                                                        <Link
+                                                            to='/signup'
+                                                            className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                                                        >
+                                                            Sign Up
+                                                        </Link>
+                                                    </>
+                                                )}
+                                            </div>
+                                        </div>
+                                    )}
+                                </div> :
+                                <Link
+                                    to='/login'
+                                >
+                                    <Button className="text-xl">Login</Button>
+                                </Link>
+                            }
                         </div>
-                      </>
-                    ) : (
-                      <>
-                        <Link
-                          to='/login'
-                          className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
-                        >
-                          Login
-                        </Link>
-                        <Link
-                          to='/signup'
-                          className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
-                        >
-                          Sign Up
-                        </Link>
-                      </>
-                    )}
-                  </div>
-                </div>
-              )}
+                    </div>
+                </Container>
             </div>
-          </div>
-        </Container>
-      </div>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Navbar
